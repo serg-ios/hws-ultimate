15d42d679efb98825a05c903f0665a67e010d8aa
Sergio Rodríguez Rama <58111203+serg-ios@users.noreply.github.com>
2021-01-25 17:23:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hws-ultimate
+Learn to build one complete app using SwiftUI, Core Data, CloudKit, StoreKit, Widgets, Spotlight, and more.
---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Sergio Rodríguez Rama on 25/1/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
