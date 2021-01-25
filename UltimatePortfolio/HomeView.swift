25bf3cfb7728559b4f0aab490c362939e1e22e2b
Sergio Rodríguez Rama <sergio.rodriguez@nomasystems.com>
2021-01-25 19:58:40 +0000

    First steps in UI.

---
+++ UltimatePortfolio/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  UltimatePortfolio
+//
+//  Created by Sergio Rodríguez Rama on 25/1/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+
+    @EnvironmentObject var dataController: DataController
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
