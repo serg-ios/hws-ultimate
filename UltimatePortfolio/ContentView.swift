283186317722ef5bcc90877f5f01027a7a238546
Sergio Rodríguez Rama <sergio.rodriguez@nomasystems.com>
2021-01-25 19:58:40 +0000

    First steps in UI.

--- UltimatePortfolio/ContentView.swift
+++ UltimatePortfolio/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            ProjectsView(showClosedProjects: false)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Open")
+                }
+            ProjectsView(showClosedProjects: true)
+                .tabItem {
+                    Image(systemName: "checkmark")
+                    Text("Close")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+
+    static var dataController = DataController.preview
+
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
     }
 }
