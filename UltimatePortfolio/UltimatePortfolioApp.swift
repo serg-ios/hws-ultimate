ff2826a309b001d1f49096172923aca7f1b96a6b
Sergio Rodríguez Rama <sergio.rodriguez@nomasystems.com>
2021-01-25 19:12:06 +0000

    Designing a great model.

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct UltimatePortfolioApp: App {
+
+    @StateObject var dataController: DataController
+
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
     }
 }
