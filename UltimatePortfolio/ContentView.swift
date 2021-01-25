d7506677a70cadbe87b1ac6c28eeb41f814d6eba
Sergio Rodríguez Rama <58111203+serg-ios@users.noreply.github.com>
2021-01-25 17:23:59 +0000

    Initial commit

---
+++ UltimatePortfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UltimatePortfolio
+//
+//  Created by Sergio Rodríguez Rama on 25/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
