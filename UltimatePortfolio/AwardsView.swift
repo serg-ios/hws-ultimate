d56e15aa0eb498e1bb3cb697fa7f2af1bbc3658e
Sergio Rodríguez Rama <sergio.rodriguez@nomasystems.com>
2021-02-04 19:19:31 +0000

    Counting Core Data results.

--- UltimatePortfolio/AwardsView.swift
+++ UltimatePortfolio/AwardsView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct AwardsView: View {
 
+    @EnvironmentObject var dataController: DataController
+
+    @State private var selectedAward = Award.example
+    @State private var showingAwardDetails = false
+
     static let tag: String? = "Awards"
 
     var body: some View {
@@ -16,14 +21,27 @@ struct AwardsView: View {
             ScrollView {
                 LazyVGrid(columns: columns) {
                     ForEach(Award.allAwards) { award in
-                        Image(systemName: award.image)
-                            .resizable()
-                            .scaledToFit()
-                            .padding()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(Color.secondary.opacity(0.5))
+                        Button {
+                            selectedAward = award
+                            showingAwardDetails = true
+                        } label: {
+                            Image(systemName: award.image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(dataController.hasEarned(award: award) ? Color(award.color) : Color.secondary.opacity(0.5))
+                        }
                     }
                 }
+            }
+            .navigationTitle("Awards")
+        }
+        .alert(isPresented: $showingAwardDetails) {
+            if dataController.hasEarned(award: selectedAward) {
+                return Alert(title: Text("Unlocked: \(selectedAward.name)"), message: Text(selectedAward.description), dismissButton: .default(Text("OK")))
+            } else {
+                return Alert(title: Text("Locked"), message: Text(selectedAward.description), dismissButton: .default(Text("OK")))
             }
         }
     }
