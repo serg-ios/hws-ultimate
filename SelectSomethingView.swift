1852e26a896bcd293f59567d1efbb6bd533f951c
Sergio Rodríguez Rama <sergio.rodriguez@nomasystems.com>
2021-02-04 17:42:50 +0000

    Finishing ProjectsView.

---
+++ SelectSomethingView.swift
@@ -0,0 +1,22 @@
+//
+//  SelectSomethingView.swift
+//  UltimatePortfolio
+//
+//  Created by Sergio Rodríguez Rama on 4/2/21.
+//
+
+import SwiftUI
+
+struct SelectSomethingView: View {
+    var body: some View {
+        Text("Please select something from the menu to begin")
+            .italic()
+            .foregroundColor(.secondary)
+    }
+}
+
+struct SelectSomethingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectSomethingView()
+    }
+}
