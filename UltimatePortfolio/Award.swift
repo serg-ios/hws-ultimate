5304ee7af640b0bd8a916b87fcea2cd0f4a34ffa
Sergio Rodríguez Rama <sergio.rodriguez@nomasystems.com>
2021-02-04 18:50:11 +0000

    Reading awards JSON.

---
+++ UltimatePortfolio/Award.swift
@@ -0,0 +1,23 @@
+//
+//  Award.swift
+//  UltimatePortfolio
+//
+//  Created by Sergio Rodríguez Rama on 4/2/21.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+
+    var id: String { name }
+
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
