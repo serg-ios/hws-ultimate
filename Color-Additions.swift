1b531c93c9addffdab45716870d1cae961975011
Sergio Rodríguez Rama <sergio.rodriguez@nomasystems.com>
2021-02-06 15:58:57 +0000

    Creating the Home View.

---
+++ Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Sergio Rodríguez Rama on 5/2/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
--- UltimatePortfolio/HomeView.swift
+++ UltimatePortfolio/HomeView.swift
@@ -5,6 +5,7 @@
 //  Created by Sergio Rodríguez Rama on 25/1/21.
 //
 
+import CoreData
 import SwiftUI
 
 struct HomeView: View {
@@ -13,15 +14,93 @@ struct HomeView: View {
 
     @EnvironmentObject var dataController: DataController
 
+    @FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
+
+    let items: FetchRequest<Item>
+
+    var projectRows: [GridItem] {
+        [GridItem(.fixed(100))]
+    }
+
+    init() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "completed = false")
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \Item.priority, ascending: false)]
+        request.fetchLimit = 10
+        items = FetchRequest(fetchRequest: request)
+    }
+
     var body: some View {
         NavigationView {
-            VStack {
-                Button("Add data") {
-                    dataController.deleteAll()
-                    try? dataController.createSampleData()
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: projectRows) {
+                            ForEach(projects) { project in
+                                VStack(alignment: .leading) {
+                                    Text("\(project.projectItems.count) items")
+                                        .font(.caption)
+                                        .foregroundColor(.secondary)
+
+                                    Text(project.projectTitle)
+                                        .font(.title2)
+
+                                    ProgressView(value: project.completionAmount)
+                                        .accentColor(Color(project.projectColor))
+                                }
+                                .padding()
+                                .background(Color.secondarySystemGroupedBackground)
+                                .cornerRadius(10)
+                                .shadow(color: Color.black.opacity(0.2), radius: 5)
+                            }
+                        }
+                        .padding([.horizontal, .top])
+                        .fixedSize(horizontal: false, vertical: true)
+                    }
+                    VStack(alignment: .leading) {
+                        list("Up next", for: items.wrappedValue.prefix(3))
+                        list("More to explore", for: items.wrappedValue.dropFirst(3))
+                    }
+                    .padding([.horizontal, .bottom])
                 }
             }
+            .background(Color.systemGroupedBackground.ignoresSafeArea())
             .navigationTitle("Home")
+        }
+    }
+
+    @ViewBuilder func list(_ title: String, for items: FetchedResults<Item>.SubSequence) -> some View {
+        if items.isEmpty {
+            EmptyView()
+        } else {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.secondary)
+                .padding(.top)
+            ForEach(items) { item in
+                NavigationLink(destination: EditItemView(item: item)) {
+                    HStack(spacing: 20) {
+                        Circle()
+                            .stroke(Color(item.project?.projectColor ?? "Light Blue"), lineWidth: 3)
+                            .frame(width: 44, height: 44)
+
+                        VStack(alignment: .leading) {
+                            Text(item.itemTitle)
+                                .font(.title2)
+                                .foregroundColor(.primary)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            if item.itemDetail.isEmpty == false {
+                                Text(item.itemDetail)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                    .padding()
+                    .background(Color.secondarySystemGroupedBackground)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5)
+                }
+            }
         }
     }
 }
