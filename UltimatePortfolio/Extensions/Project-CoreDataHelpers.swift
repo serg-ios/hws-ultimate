c7fa9a724df11b169c4388f2c4f19e6c77c9a7af
Sergio Rodríguez Rama <sergio.rodriguez@nomasystems.com>
2021-02-14 11:02:55 +0000

    Testing development data.

--- UltimatePortfolio/Extensions/Project-CoreDataHelpers.swift
+++ UltimatePortfolio/Extensions/Project-CoreDataHelpers.swift
@@ -63,7 +63,7 @@ extension Project {
     }
 
     static var example: Project {
-        let controller = DataController(inMemory: true)
+        let controller = DataController.preview
         let viewContext = controller.container.viewContext
         let project = Project(context: viewContext)
         project.title = "Example project"
