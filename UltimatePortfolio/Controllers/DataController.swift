5bc996733e78b188d34450fb6dd29b73d8049578
Sergio Rodríguez Rama <ram.rod.ser+github@gmail.com>
2021-04-17 19:53:02 +0100

    Adding local notifications.

--- UltimatePortfolio/Controllers/DataController.swift
+++ UltimatePortfolio/Controllers/DataController.swift
@@ -8,6 +8,7 @@
 import CoreSpotlight
 import CoreData
 import SwiftUI
+import UserNotifications
 
 class DataController: ObservableObject {
     let container: NSPersistentCloudKitContainer
@@ -115,7 +116,7 @@ class DataController: ObservableObject {
             let awardCount = count(for: fetchRequest)
             return awardCount >= award.value
         default:
-//            fatalError("Unknown award criterion: \(award.criterion)")
+            //            fatalError("Unknown award criterion: \(award.criterion)")
             return false
         }
     }
@@ -141,5 +142,66 @@ class DataController: ObservableObject {
             return nil
         }
         return try? container.viewContext.existingObject(with: id) as? Item
+    }
+
+    func addReminders(for project: Project, completion: @escaping (Bool) -> Void) {
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings { [weak self] settings in
+            switch settings.authorizationStatus {
+            case .notDetermined:
+                self?.requestNotifications { [weak self] success in
+                    if success {
+                        self?.placeReminders(for: project, completion: completion)
+                    } else {
+                        DispatchQueue.main.async {
+                            completion(false)
+                        }
+                    }
+                }
+            case .authorized:
+                self?.placeReminders(for: project, completion: completion)
+            default:
+                DispatchQueue.main.async {
+                    completion(false)
+                }
+            }
+        }
+    }
+
+    func removeReminders(for project: Project) {
+        let center = UNUserNotificationCenter.current()
+        let id = project.objectID.uriRepresentation().absoluteString
+        center.removePendingNotificationRequests(withIdentifiers: [id])
+    }
+
+    private func requestNotifications(completion: @escaping (Bool) -> Void) {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { success, _ in
+            completion(success)
+        }
+    }
+
+    private func placeReminders(for project: Project, completion: @escaping (Bool) -> Void) {
+        let content = UNMutableNotificationContent()
+        content.title = project.projectTitle
+        content.sound = .default
+        if let projectDetail = project.detail {
+            content.subtitle = projectDetail
+        }
+        let components = Calendar.current.dateComponents([.hour, .minute], from: project.reminderTime ?? Date())
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
+        let id = project.objectID.uriRepresentation().absoluteString
+        let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
+
+        UNUserNotificationCenter.current().add(request) { error in
+            DispatchQueue.main.async {
+                if error == nil {
+                    completion(true)
+                } else {
+                    completion(false)
+                }
+            }
+        }
+
     }
 }
